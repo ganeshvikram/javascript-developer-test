c039c3131a59c1d1ebac70eb64a7f1b2aaf98eed
ganeshvikram <ganeshvikram08@gmail.com>
2025-09-17 15:00:24 +1000

    arine-quutes-function-added

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -2,7 +2,21 @@ const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
   // TODO: Implement this function.
-  // return results;
+
+  let results = [];
+
+  const urlResults =  urls.map(async (link)=>{
+
+      //Call httpGet from interface
+      const result = await httpGet(link); 
+      const body = JSON.parse(result.body).message;
+
+      (result.status == 200) ? results.push({"Arnie Quote":body}) : results.push({"FAILURE":body});
+
+  })
+  //Wait to resolve all promises
+   await Promise.all(urlResults);
+   return results;
 };
 
 module.exports = {
